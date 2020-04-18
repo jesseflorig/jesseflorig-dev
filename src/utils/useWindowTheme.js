edebf4970ce1a0618f24c0cad826fe8087ec4a3d
Jesse Florig <jesseflorig@gmail.com>
2020-04-18 19:01:13 -0400

    protect window and document calls

--- src/utils/useWindowTheme.js
+++ src/utils/useWindowTheme.js
@@ -1,19 +1,29 @@
 import { useState } from "react"
+const DEFAULT_THEME = "dark"
 const LOCAL_THEME_KEY = "florigPreferredTheme"
 
 function getTheme() {
-  return window.localStorage.getItem(LOCAL_THEME_KEY)
+  return typeof window !== `undefined`
+    ? window.localStorage.getItem(LOCAL_THEME_KEY)
+    : DEFAULT_THEME
 }
 
-export default function useWindowTheme(defaultTheme = "dark") {
+export default function useWindowTheme(defaultTheme = DEFAULT_THEME) {
   const [theme, setTheme] = useState(getTheme() || defaultTheme)
-  document.body.className = theme
+
+  if (typeof document !== `undefined`) {
+    document.body.className = theme
+  }
 
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark"
     setTheme(newTheme)
-    window.localStorage.setItem(LOCAL_THEME_KEY, newTheme)
-    document.body.className = newTheme
+    if (typeof window !== `undefined`) {
+      window.localStorage.setItem(LOCAL_THEME_KEY, newTheme)
+    }
+    if (typeof document !== `undefined`) {
+      document.body.className = newTheme
+    }
   }
 
   return [theme, toggleTheme]
