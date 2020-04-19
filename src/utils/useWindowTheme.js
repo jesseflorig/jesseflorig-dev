7d72361bd0fe7b9a2df75e9614597799f08d5143
Jesse Florig <jesseflorig@gmail.com>
2020-04-19 13:04:23 -0400

    update theme hook logic

--- src/utils/useWindowTheme.js
+++ src/utils/useWindowTheme.js
@@ -1,29 +1,38 @@
 import { useState } from "react"
-const DEFAULT_THEME = "dark"
-const LOCAL_THEME_KEY = "florigPreferredTheme"
 
-function getTheme() {
+const LOCAL_THEME_KEY = "florigPreferredTheme"
+const DEFAULT_THEME = "dark"
+const ALTERNATE_THEME = "light"
+
+function getLocalTheme() {
   return typeof window !== `undefined`
     ? window.localStorage.getItem(LOCAL_THEME_KEY)
     : DEFAULT_THEME
 }
 
-export default function useWindowTheme(defaultTheme = DEFAULT_THEME) {
-  const [theme, setTheme] = useState(getTheme() || defaultTheme)
-
-  if (typeof document !== `undefined`) {
-    document.body.className = theme
+function setLocalTheme(newTheme) {
+  //
+  if (typeof window !== `undefined`) {
+    window.localStorage.setItem(LOCAL_THEME_KEY, newTheme)
   }
+}
+
+function setBodyClass(newClass) {
+  //
+  if (typeof document !== `undefined`) {
+    document.body.className = newClass
+  }
+}
+
+export default function useWindowTheme(defaultTheme = DEFAULT_THEME) {
+  const [theme, setTheme] = useState(getLocalTheme() || defaultTheme)
+  setBodyClass(theme)
 
   const toggleTheme = () => {
-    const newTheme = theme === "dark" ? "light" : "dark"
+    const newTheme = theme === DEFAULT_THEME ? ALTERNATE_THEME : DEFAULT_THEME
     setTheme(newTheme)
-    if (typeof window !== `undefined`) {
-      window.localStorage.setItem(LOCAL_THEME_KEY, newTheme)
-    }
-    if (typeof document !== `undefined`) {
-      document.body.className = newTheme
-    }
+    setLocalTheme(newTheme)
+    setBodyClass(newTheme)
   }
 
   return [theme, toggleTheme]
