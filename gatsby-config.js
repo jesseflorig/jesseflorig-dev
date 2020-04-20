c2430f08fef76a9efb958218326fa9d96979a418
Jesse Florig <jesseflorig@gmail.com>
2020-04-19 21:40:59 -0400

    added analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-47568220-3`,
       },
     },
     {
